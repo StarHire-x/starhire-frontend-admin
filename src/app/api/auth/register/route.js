24776854bfa8e71d2a5daa5ab94ba06e4ba67ef5
Deshun99 <e0560014@u.nus.edu>
2023-08-31 23:43:14 +0800

    login register dashboard skeleton done

---
+++ src/app/api/auth/register/route.js
@@ -0,0 +1,22 @@
+import bcrypt from "bcryptjs";
+import { NextResponse } from "next/server";
+
+export const hashing = async (password) => {
+    return await bcrypt.hash(password, 5);
+}
+
+export const registerUser = async (request) => {
+    try {
+        const res = await fetch(
+            "http://localhost:8080/users", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(request),
+            });
+        return res;
+    } catch(err) {
+        return new NextResponse("Failed to create user", { status: 500 })
+    }
+}
