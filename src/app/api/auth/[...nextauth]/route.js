c9e7501540729d729521df8beb3c781ec2972e3d
Deshun99 <e0560014@u.nus.edu>
2023-09-06 20:29:53 +0800

    register done, login cannot retrieve role

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -10,34 +10,82 @@ const handler = NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
+    // CredentialsProvider({
+    //   id: "credentials",
+    //   name: "Credentials",
+    //   async authorize(credentials) {
+    //     console.log(credentials.email);
+    //     try {
+    //       const res = await fetch(
+    //         `http://localhost:8080/users/?email=${credentials.email}`,
+    //         {
+    //           cache: "no-store",
+    //         }
+    //       );
+
+    //       if (!res.ok) {
+    //         return notFound();
+    //       }
+
+    //       const responseBody = await res.json(); // Read the response body once
+
+    //       const isPasswordCorrect = await bcrypt.compare(
+    //         credentials.password,
+    //         responseBody.password
+    //       );
+
+    //       if (isPasswordCorrect) {
+    //         return responseBody;
+    //       } else {
+    //         throw new Error("Wrong Credentials!");
+    //       }
+    //     } catch (err) {
+    //       throw new Error(err);
+    //     }
+    //   },
+    // }),
     CredentialsProvider({
-      id: "credentials",
-      name: "Credentials",
-      async authorize(credentials) {
-        console.log(credentials.email);
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+        role: { label: "Role", type: "radio" },
+      },
+      authorize: async (credentials) => {
         try {
+          const { email, password, role } = credentials;
           const res = await fetch(
-            `http://localhost:8080/users/?email=${credentials.email}`,
+            `http://localhost:8080/users?email=${email}&role=${role}`,
             {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+              },
               cache: "no-store",
             }
           );
-    
-          if (!res.ok) {
-            return notFound();
-          }
-    
-          const responseBody = await res.json(); // Read the response body once
-    
-          const isPasswordCorrect = await bcrypt.compare(
-            credentials.password,
-            responseBody.password
-          );
-    
-          if (isPasswordCorrect) {
-            return responseBody;
+          if (res.status === 200) {
+            const userData = await res.json();
+            const hashedPasswordFromDB = userData.password; // Replace 'password' with the actual field name in your database
+            const passwordsMatch = await bcrypt.compare(
+              password,
+              hashedPasswordFromDB
+            );
+
+            if (passwordsMatch) {
+              // If the passwords match, return the user object
+              return Promise.resolve({
+                id: userData.userId,
+                name: userData.userName,
+                email: userData.email,
+                role: userData.role,
+              });
+            } else {
+              throw new Error("Wrong Credentials!"); // Authentication failed
+            }
+          } else if (res.status === 404) {
+            return notFound(); // User not found
           } else {
-            throw new Error("Wrong Credentials!");
+            return notFound(); // Error fetching user
           }
         } catch (err) {
           throw new Error(err);
