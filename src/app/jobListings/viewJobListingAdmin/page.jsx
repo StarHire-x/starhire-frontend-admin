540177481d7a9728c320e4ff95bcab938278116d
Alwin Ng Jun Wei <alwinngjw@gmail.com>
2023-09-12 21:39:52 +0800

    added logic to handle confirming mehcanism

--- src/app/jobListings/viewJobListingAdmin/page.jsx
+++ src/app/jobListings/viewJobListingAdmin/page.jsx
@@ -11,7 +11,6 @@ import { Dialog } from "primereact/dialog";
 import { useSession } from "next-auth/react";
 
 export default function ViewJobListingAdmin() {
-
   const session = useSession();
 
   const router = useRouter();
@@ -25,8 +24,7 @@ export default function ViewJobListingAdmin() {
 
   const [userDialog, setUserDialog] = useState(false);
 
-  const [selectedRowData, setSelectedRowData] = useState(null);
-
+  const [status, setStatus] = useState(null);
 
   useEffect(() => {
     fetch(`http://localhost:8080/job-listing/${id}`)
@@ -46,28 +44,35 @@ export default function ViewJobListingAdmin() {
       });
   }, []);
 
+  /*
   const header = (
     <img
       alt="Card"
       src="https://primefaces.org/cdn/primereact/images/usercard.png"
     />
   );
+  */
+
+  /*
+  <div className="p-col">
+                <strong>Listing Date:</strong>
+                <p>{new Date(jobListing.listingDate).toLocaleDateString()}</p>
+              </div>
+              */
 
   const handleRefresh = () => {
     router.push(`/jobListings`); // This will refresh the current page
   };
 
-
   //API call
   const updateJobListingStatusAPICall = async (request, id) => {
     try {
-      const res = await fetch(`http://localhost:8080/job-listing/${id}`, 
-      {
+      const res = await fetch(`http://localhost:8080/job-listing/${id}`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(request)
+        body: JSON.stringify(request),
       });
 
       console.log(res);
@@ -81,23 +86,21 @@ export default function ViewJobListingAdmin() {
       console.log("There was a problem", error);
       throw error;
     }
-}
+  };
 
   const updateJobListingStatus = async (newStatus) => {
     try {
       const request = {
-        jobListingStatus: newStatus
+        jobListingStatus: newStatus,
       };
-      const response = await updateJobListingStatusAPICall(
-        request,
-        id
-      );
+      const response = await updateJobListingStatusAPICall(request, id);
       console.log("Status changed successfully:", response);
     } catch (error) {
       console.error("Error changing status:", error);
     }
   };
 
+  /*
   const footer = (
     <div className="flex flex-wrap justify-content-end gap-2">
       <Button
@@ -120,6 +123,46 @@ export default function ViewJobListingAdmin() {
       />
     </div>
   );
+  */
+ const hideDialog = () => {
+    setUserDialog(false);
+  };
+
+  const showUserDialog = (action) => {
+    setUserDialog(true);
+    setStatus(action);
+
+  };
+
+  const userDialogFooter = (
+    <React.Fragment>
+      <Button label="Cancel" icon="pi pi-times" outlined onClick={hideDialog} />
+      <Button label="Yes" icon="pi pi-check" outlined onClick={() => updateJobListingStatus(status)} />
+    </React.Fragment>
+  );
+
+  const footer = (
+    <div className="flex flex-wrap justify-content-end gap-2">
+      <Button
+        label="Approve"
+        icon="pi pi-check"
+        className="approve-button p-button-outlined p-button-secondary"
+        onClick={() => showUserDialog("Active")}
+      />
+      <Button
+        label="Reject"
+        icon="pi pi-times"
+        className="reject-button p-button-outlined p-button-secondary"
+        onClick={() => showUserDialog("Inactive")}
+      />
+      <Button
+        label="Archive"
+        icon="pi pi-times"
+        className="archive-button p-button-outlined p-button-secondary"
+        onClick={() => showUserDialog("Unverified")}
+      />
+    </div>
+  );
 
 
   return (
@@ -130,39 +173,29 @@ export default function ViewJobListingAdmin() {
         </div>
       ) : (
         <div>
-          <Card title={jobListing.title} footer={footer} header={header}>
-            <div className="p-grid">
-              <div className="p-col">
-                <strong>Description:</strong>
-                <p>{jobListing.description}</p>
-              </div>
-              <div className="p-col">
-                <strong>Job Location:</strong>
-                <p>{jobListing.jobLocation}</p>
-              </div>
-              <div className="p-col">
-                <strong>Listing Date:</strong>
-                <p>{new Date(jobListing.listingDate).toLocaleDateString()}</p>
-              </div>
-              <div className="p-col">
-                <strong>Average Salary:</strong>
-                <p>{jobListing.averageSalary}</p>
-              </div>
-              <div className="p-col">
-                <strong>Job Start Date:</strong>
-                <p>{new Date(jobListing.jobStartDate).toLocaleDateString()}</p>
-              </div>
-              <div className="p-col">
-                <strong>Job Listing Status:</strong>
-                <p>{jobListing.jobListingStatus}</p>
-              </div>
-              <div className="p-col">
-                <strong>Corporate:</strong>
-                <p>{jobListing.corporate.companyName}</p>
-              </div>
+          <Card
+            title={jobListing.title}
+            subTitle={jobListing.jobLocation}
+            footer={footer}
+            className="my-card"
+            style={{ borderRadius: "0" }}
+          >
+            <div className="my-card.p-card-content">
+              <strong>Job Desription</strong>
+              <p>{jobListing.description}</p>
             </div>
-
           </Card>
+          
+          <Dialog
+            visible={userDialog}
+            style={{ width: "32rem" }}
+            breakpoints={{ "960px": "75vw", "641px": "90vw" }}
+            header="Confirm?"
+            className="p-fluid"
+            footer={userDialogFooter}
+            onHide={hideDialog}
+          >
+          </Dialog>
         </div>
       )}
     </div>
