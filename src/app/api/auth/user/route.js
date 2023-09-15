ca313cc1b4e1f6537604ad3ed5fa3622e10fd1be
Deshun99 <e0560014@u.nus.edu>
2023-09-15 12:52:52 +0800

    edited the logic to get a user, via userId Instead

--- src/app/api/auth/user/route.js
+++ src/app/api/auth/user/route.js
@@ -92,3 +92,29 @@ export const getUserByEmailRole = async (email, role) => {
     throw error;
   }
 };
+
+export const getUserByUserId = async (userId, role, accessToken) => {
+  try {
+    const res = await fetch(
+      `http://localhost:8080/users/search?userId=${userId}&role=${role}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+        cache: "no-store",
+      }
+    );
+    const responseBody = await res.json();
+
+    if (responseBody.statusCode === 404) {
+      throw new Error(responseBody.message || "An error occurred");
+    }
+    return await responseBody;
+  } catch (error) {
+    console.log("There was a problem fetching the users", error);
+    throw error;
+  }
+};
+
