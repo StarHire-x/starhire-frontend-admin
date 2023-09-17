cfd7926c6d553f0b7b37701a0c6482c8ad5fd892
Alwin Ng Jun Wei <alwinngjw@gmail.com>
2023-09-17 12:05:12 +0800

    formatted code

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -91,66 +91,73 @@ const Navbar = () => {
       <Link href="/" className={styles.logo}>
         StarHire
       </Link>
-      
+
       <div className={styles.links}>
         <DarkModeToggle />
-        {session.status === "authenticated" && session.data.user.role === "Administrator" && adminLinks.map((link) => (
-          <div
-            key={link.id}
-            className={styles.linkContainer}
-            onMouseEnter={() => handleLinkMouseEnter(link.id)}
-            onMouseLeave={handleLinkMouseLeave}
-          > 
-          <Link href={link.url} className={styles.link}>
-              {link.title}
-            </Link>
-            {link.submenu && showDropdown === link.id && (
-              <div className={styles.dropdown}>
-                {link.submenu.map((submenuItem) => (
-                  <Link
-                    key={submenuItem.id}
-                    href={submenuItem.url}
-                    className={styles.submenuItem}
-                  >
-                    {submenuItem.title}
-                  </Link>
-                ))}
-              </div>
-            )}
-          </div>
-        ))}
-        {session.status === "authenticated" && session.data.user.role === "Recruiter" && recruiterLinks.map((link) => (
-          <div
-            key={link.id}
-            className={styles.linkContainer}
-            onMouseEnter={() => handleLinkMouseEnter(link.id)}
-            onMouseLeave={handleLinkMouseLeave}
-          > 
-          <Link href={link.url} className={styles.link}>
-              {link.title}
-            </Link>
-            {link.submenu && showDropdown === link.id && (
-              <div className={styles.dropdown}>
-                {link.submenu.map((submenuItem) => (
-                  <Link
-                    key={submenuItem.id}
-                    href={submenuItem.url}
-                    className={styles.submenuItem}
-                  >
-                    {submenuItem.title}
-                  </Link>
-                ))}
-              </div>
-            )}
-          </div>
-        ))}
+        {session.status === "authenticated" &&
+          session.data.user.role === "Administrator" &&
+          adminLinks.map((link) => (
+            <div
+              key={link.id}
+              className={styles.linkContainer}
+              onMouseEnter={() => handleLinkMouseEnter(link.id)}
+              onMouseLeave={handleLinkMouseLeave}
+            >
+              <Link href={link.url} className={styles.link}>
+                {link.title}
+              </Link>
+              {link.submenu && showDropdown === link.id && (
+                <div className={styles.dropdown}>
+                  {link.submenu.map((submenuItem) => (
+                    <Link
+                      key={submenuItem.id}
+                      href={submenuItem.url}
+                      className={styles.submenuItem}
+                    >
+                      {submenuItem.title}
+                    </Link>
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
+        {session.status === "authenticated" &&
+          session.data.user.role === "Recruiter" &&
+          recruiterLinks.map((link) => (
+            <div
+              key={link.id}
+              className={styles.linkContainer}
+              onMouseEnter={() => handleLinkMouseEnter(link.id)}
+              onMouseLeave={handleLinkMouseLeave}
+            >
+              <Link href={link.url} className={styles.link}>
+                {link.title}
+              </Link>
+              {link.submenu && showDropdown === link.id && (
+                <div className={styles.dropdown}>
+                  {link.submenu.map((submenuItem) => (
+                    <Link
+                      key={submenuItem.id}
+                      href={submenuItem.url}
+                      className={styles.submenuItem}
+                    >
+                      {submenuItem.title}
+                    </Link>
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
         {session.status === "authenticated" && (
           <button className={styles.logout} onClick={signOut}>
             Logout
           </button>
         )}
         {session.status === "unauthenticated" && (
-          <button className={styles.login} onClick={() => window.location.href = "/login"}>
+          <button
+            className={styles.login}
+            onClick={() => (window.location.href = "/login")}
+          >
             Login
           </button>
         )}
