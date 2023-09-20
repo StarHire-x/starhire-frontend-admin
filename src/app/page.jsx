f70b1f9ca00fa25095fb1199a191048d8adaa7e1
rilwan99 <rilwanwild@gmail.com>
2023-09-20 18:08:58 +0800

    inlcude register button

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from 'next/image'
 import styles from './page.module.css'
 import Hero from 'public/hero.png'
@@ -9,7 +10,21 @@ export default function Home() {
       <div className={styles.item}>
         <h1 className={styles.title}>StarHire Administrator Portal</h1>
         <p className={styles.desc}>Explore our services and manage your operations with ease.</p>
-        <Button url="/login" text="Login"/>
+        <div className={styles.buttonContainer}>
+          <button
+            className={styles.register}
+            onClick={() => (window.location.href = "/register")}
+          >
+            Register
+          </button>
+          
+          <button
+            className={styles.login}
+            onClick={() => (window.location.href = "/login")}
+          >
+            Login
+          </button>
+        </div>
       </div>
       <div className={styles.item}></div>
       <Image src={Hero} alt="AltPhoto" className={styles.img}/>
