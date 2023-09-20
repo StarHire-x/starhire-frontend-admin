a5d87db9738931046acb9c6ab1478651cfb9f2f5
hengyjj <hengyjj@hotmail.com>
2023-09-20 09:14:06 +0800

    portover the registration page from client to admin portal

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,5 +1,4 @@
 "use client";
-
 import React from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
@@ -8,19 +7,10 @@ import { useState } from "react";
 import { hashing } from "@/app/api/auth/register/route";
 import { registerUser } from "@/app/api/auth/register/route";
 
-const Register = () => {
-  const [err, setErr] = useState(false);
-
-  const [formData, setFormData] = useState({
-    userName: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-    contactNumber: "",
-    role: "",
-  })
-
-  const router = useRouter();
+const Step1 = ({ formData, setFormData, onNext }) => {
+  const handleNext = () => {
+    onNext();
+  };
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -28,6 +18,182 @@ const Register = () => {
       ...formData,
       [name]: value,
     });
+  };
+
+  return (
+    <div className={styles.container}>
+      <h2>Step 1: User Information</h2>
+      <form className={styles.form}>
+        <div className={styles.userRole}>
+          <div>
+            <p>I am registering as a...</p>
+          </div>
+
+          <div className={styles.radio}>
+            <label>
+              <input
+                type="radio"
+                name="role"
+                value="Administrator"
+                checked={formData.role === "Administrator"}
+                onChange={handleInputChange}
+              />
+              Administrator
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="role"
+                value="Recruiter"
+                checked={formData.role === "Recruiter"}
+                onChange={handleInputChange}
+              />
+              Recruiter
+            </label>
+          </div>
+        </div>
+
+        <div className={styles.inputFields}>
+          <input
+            type="text"
+            name="userName"
+            placeholder="Username"
+            className={styles.input}
+            value={formData.userName}
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            className={styles.input}
+            value={formData.email}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+      </form>
+      <button className={styles.button} onClick={handleNext}>Next</button>
+    </div>
+  );
+};
+
+const Step2 = ({ formData, setFormData, onNext, onPrevious }) => {
+  const handleNext = () => {
+    onNext();
+  };
+
+  const handlePrevious = () => {
+    onPrevious();
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  return (
+    <div className={styles.container}>
+      <h2>Step 2: Password</h2>
+      <form className={styles.form}>
+        <div className={styles.inputFields}>
+        <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            className={styles.input}
+            value={formData.password}
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirm password"
+            className={styles.input}
+            value={formData.confirmPassword}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+      </form>
+      <div className={styles.stepTwoThreeButton}>
+        <button className={styles.button} onClick={handlePrevious}>Previous</button>
+        <div className={styles.spacer}></div>
+        <button className={styles.button} onClick={handleNext}>Next</button>
+      </div>
+    </div>
+  );
+};
+
+const Step3 = ({ formData, setFormData, onPrevious, onSubmit, err }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform final validation and registration logic here
+    onSubmit(e);
+  };
+
+  const handlePrevious = () => {
+    onPrevious();
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  return (
+    <div className={styles.container}>
+      <h2>Step 3: Additional Information</h2>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div className={styles.inputFields}>
+        <input
+            type="text"
+            name="contactNumber"
+            placeholder="Contact Number"
+            className={styles.input}
+            value={formData.contactNumber}
+            onChange={handleInputChange}
+          />
+        </div>
+        {err && "Something went wrong!"}
+        <div className={styles.stepTwoThreeButton}>
+          <button className={styles.button} onClick={handlePrevious}>Previous</button>
+          <div className={styles.spacer}></div>
+          <button className={styles.button}>Register</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+const Register = () => {
+  const router = useRouter();
+
+  const [err, setErr] = useState(false);
+  const [currentStep, setCurrentStep] = useState(1);
+  const [formData, setFormData] = useState({
+    userName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    contactNumber: "",
+    role: "",
+  });
+
+  const handleNext = () => {
+    setCurrentStep(currentStep + 1);
+  };
+
+  const handlePrevious = () => {
+    setCurrentStep(currentStep - 1);
   };
 
   const handleSubmit = async (e) => {
@@ -42,11 +208,18 @@ const Register = () => {
       return; // Exit early if passwords don't match
     }
 
-    // const hashedPassword = await hashing(formData.password);
-    if (!formData.userName || !formData.email || !formData.contactNumber || !formData.role) {
-      alert("Please ensure you have filled all the fields, especially your role.");
+    if (
+      !formData.userName ||
+      !formData.email ||
+      !formData.contactNumber ||
+      !formData.role
+    ) {
+      alert(
+        "Please ensure you have filled all the fields, especially your role."
+      );
       return;
     }
+
     const data = {
       userName: formData.userName,
       email: formData.email,
@@ -68,77 +241,30 @@ const Register = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Registration</h1>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="userName"
-          placeholder="Username"
-          className={styles.input}
-          value={formData.userName}
-          onChange={handleInputChange}
-          required
+      {currentStep === 1 && (
+        <Step1
+          formData={formData}
+          setFormData={setFormData}
+          onNext={handleNext}
         />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          className={styles.input}
-          value={formData.email}
-          onChange={handleInputChange}
-          required
+      )}
+      {currentStep === 2 && (
+        <Step2
+          formData={formData}
+          setFormData={setFormData}
+          onNext={handleNext}
+          onPrevious={handlePrevious}
         />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          className={styles.input}
-          value={formData.password}
-          onChange={handleInputChange}
-          required
+      )}
+      {currentStep === 3 && (
+        <Step3
+          formData={formData}
+          setFormData={setFormData}
+          onPrevious={handlePrevious}
+          onSubmit={handleSubmit}
+          err = {err}
         />
-        <input
-          type="password"
-          name="confirmPassword"
-          placeholder="Confirm password"
-          className={styles.input}
-          value={formData.confirmPassword}
-          onChange={handleInputChange}
-          required
-        />
-        <input
-          type="text"
-          name="contactNumber"
-          placeholder="Contact Number"
-          className={styles.input}
-          value={formData.contactNumber}
-          onChange={handleInputChange}
-          required
-        />
-        <div className={styles.radio}>
-          <label>
-            <input
-              type="radio"
-              name="role"
-              value="Administrator"
-              checked={formData.role === "Administrator"}
-              onChange={handleInputChange}
-            />
-            Administrator
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="role"
-              value="Recruiter"
-              checked={formData.role === "Recruiter"}
-              onChange={handleInputChange}
-            />
-            Recruiter
-          </label>
-        </div>
-        <button className={styles.button}>Register</button>
-        {err && "Something went wrong!"}
-      </form>
+      )}
       <Link href="/login">Login with an existing account</Link>
       <Link href="/forgetPassword">Forget Password</Link>
     </div>
