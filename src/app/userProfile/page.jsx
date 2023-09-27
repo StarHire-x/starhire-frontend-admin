1a0313cf5de33f7abba2c6e718236281a6297308
Lukas Lee <jiawei3e1@gmail.com>
2023-09-27 20:34:08 +0800

    finished user profile page and refactored some codes

--- src/app/userProfile/page.jsx
+++ src/app/userProfile/page.jsx
@@ -7,10 +7,8 @@ import { getUserByUserId } from "../api/auth/user/route";
 import HumanIcon from "../../../public/icon.png";
 import styles from "./page.module.css";
 import { Card } from "primereact/card";
-import { Panel } from "primereact/panel";
-import { Button } from "primereact/button";
-import { Dialog } from "primereact/dialog";
-import { Rating } from "primereact/rating";
+import JobExperiencePanel from "@/components/JobExperiencePanel/JobExperiencePanel";
+import JobPreferencePanel from "@/components/JobPreferencePanel/JobPreferencePanel";
 
 export default function UserProfile() {
   const session = useSession();
@@ -20,9 +18,6 @@ export default function UserProfile() {
   const [user, setUser] = useState({});
 
   const [isLoading, setIsLoading] = useState(true);
-
-  // Job Preference informaton Dialog Box
-  const [visible, setVisible] = useState(false);
 
   const userIdRef =
     session.status === "authenticated" &&
@@ -69,30 +64,6 @@ export default function UserProfile() {
     return new Date(dateString).toLocaleDateString(undefined, options);
   };
 
-  const headerTemplate = (options) => {
-    const className = `${options.className}`;
-    const titleClassName = `${options.titleClassName} ml-2 text-primary`;
-    const style = { fontSize: "1.25rem" };
-
-    return (
-      <div className={className}>
-        <span className={titleClassName} style={style}>
-          Job Preferences
-        </span>
-        <Button
-          style={{
-            width: "30px",
-            height: "30px",
-          }}
-          severity="info"
-          onClick={() => setVisible(true)}
-          icon="pi pi-info"
-          outlined
-        ></Button>
-      </div>
-    );
-  };
-
   return (
     <div className={styles.container}>
       {isLoading ? (
@@ -129,142 +100,10 @@ export default function UserProfile() {
             </Card>
           </div>
           <div className={styles.jobPreferenceSection}>
-            <Panel headerTemplate={headerTemplate}>
-              <div className={styles.dialogueContainer}>
-                {/* <Button
-                  style={{
-                    width: "30px",
-                    height: "30px",
-                    marginBottom: "30px",
-                  }}
-                  severity="info"
-                  onClick={() => setVisible(true)}
-                  icon="pi pi-info"
-                  outlined
-                ></Button> */}
-                <Dialog
-                  header="What are job preferences?"
-                  visible={visible}
-                  style={{ width: "80vw" }}
-                  onHide={() => setVisible(false)}
-                >
-                  <p className={styles.dialogueText}>
-                    These preferences serve as indicators of your prioritization
-                    criteria when evaluating potential job opportunities.
-                    <br />
-                    <br />
-                    You will be matched to suitable opportunities based on the
-                    preferences that you have provided
-                    <br />
-                    <br />
-                    Locations:
-                    <br />5 star: {"<"} 1km of mrt/bus
-                    <br />4 star: {"<"} 2km of mrt/bus
-                    <br />3 star: {"<"} 3km of mrt/bus
-                    <br />2 star: {"<"} 5km of mrt/bus
-                    <br />1 star: {"<"} 10km of mrt/bus
-                    <br />
-                    <br />
-                    Salary:
-                    <br />5 star: {">"}$10,000
-                    <br />4 star: {">"}$5,000
-                    <br />3 star: {">"}$3,500
-                    <br />2 star: {">"}$2,500
-                    <br />1 star: {">"}$1,500
-                    <br />
-                    <br />
-                    Work Life Balance:
-                    <br />5 star: {">"}50 hrs / week
-                    <br />4 star: {">"}40 hrs / week
-                    <br />3 star: {">"}30 hrs / week
-                    <br />2 star: {">"}20 hrs / week
-                    <br />1 star: {">"}10 hrs / week
-                    <br />
-                  </p>
-                </Dialog>
-              </div>
-              <div className={styles.inputFields}>
-                <div className={styles.fieldRating}>
-                  <label
-                    htmlFor="locationPreference"
-                    className={styles.labelRating}
-                  >
-                    Location:
-                  </label>
-                  <Rating
-                    value={Number(user?.jobPreference?.locationPreference)}
-                    disabled={true}
-                    stars={5}
-                    cancel={false}
-                  />
-                </div>
-
-                <div className={styles.fieldRating}>
-                  <label
-                    htmlFor="salaryPreference"
-                    className={styles.labelRating}
-                  >
-                    Salary:
-                  </label>
-                  <Rating
-                    value={Number(user?.jobPreference?.salaryPreference)}
-                    stars={5}
-                    disabled={true}
-                    cancel={false}
-                  />
-                </div>
-
-                <div className={styles.fieldRating}>
-                  <label
-                    htmlFor="workLifeBalancePreference"
-                    className={styles.labelRating}
-                  >
-                    Work Life Balance:
-                  </label>
-                  <Rating
-                    value={Number(
-                      user?.jobPreference?.workLifeBalancePreference
-                    )}
-                    stars={5}
-                    disabled={true}
-                    cancel={false}
-                  />
-                </div>
-
-                <div className={styles.fieldRating}>
-                  <label
-                    htmlFor="culturePreference"
-                    className={styles.labelRating}
-                  >
-                    Culture:
-                  </label>
-                  <Rating
-                    value={Number(user?.jobPreference?.culturePreference)}
-                    stars={5}
-                    disabled={true}
-                    cancel={false}
-                  />
-                </div>
-
-                <div className={styles.fieldRating}>
-                  <label
-                    htmlFor="diversityPreference"
-                    className={styles.labelRating}
-                  >
-                    Diversity:
-                  </label>
-                  <Rating
-                    value={Number(user?.jobPreference?.diversityPreference)}
-                    stars={5}
-                    disabled={true}
-                    cancel={false}
-                  />
-                </div>
-              </div>
-            </Panel>
+            <JobPreferencePanel jobPreference={user?.jobPreference} />
           </div>
           <div className={styles.jobExperienceSection}>
-            <Panel header="Job Experience"></Panel>
+            <JobExperiencePanel jobExperience={user?.jobExperiences} />
           </div>
         </>
       )}
---
+++ src/components/JobExperiencePanel/JobExperiencePanel.jsx
@@ -0,0 +1,105 @@
+import { Dropdown } from "primereact/dropdown";
+import styles from "./JobExperiencePanel.module.css";
+import { Card } from "primereact/card";
+import { Panel } from "primereact/panel";
+import { DataView } from "primereact/dataview";
+import { useState } from "react";
+
+const JobExperiencePanel = ({ jobExperience }) => {
+  const [sortKey, setSortKey] = useState(null);
+  const [sortOrder, setSortOrder] = useState(null);
+
+  const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "2-digit", day: "2-digit" };
+    return new Date(dateString).toLocaleDateString("en-GB", options);
+  };
+
+  const jobExperienceHeader = (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <h2 className={styles.headerTitle}>Job Experiences</h2>
+      <div>
+        <Dropdown
+          value={sortKey}
+          options={[
+            { label: "Start Date", value: "startDate" },
+            { label: "Job Title", value: "jobTitle" },
+          ]}
+          onChange={(e) => setSortKey(e.value)}
+          placeholder="Sort By"
+        />
+        <Dropdown
+          value={sortOrder}
+          options={[
+            { label: "Asc", value: 1 },
+            { label: "Desc", value: -1 },
+          ]}
+          onChange={(e) => setSortOrder(e.value)}
+          placeholder="Order"
+        />
+      </div>
+    </div>
+  );
+
+  const sortFunction = (data) => {
+    if (sortKey && sortOrder) {
+      return [...data].sort((a, b) => {
+        const value1 = a[sortKey];
+        const value2 = b[sortKey];
+        if (value1 < value2) return -1 * sortOrder;
+        if (value1 > value2) return 1 * sortOrder;
+        return 0;
+      });
+    }
+    return data;
+  };
+
+  const itemTemplate = (jobExperience) => {
+    return (
+      <Card className={styles.card}>
+        <div className={styles.cardHeader}>
+          <div className={styles.cardHeaderLeft}>
+            <h4>{jobExperience.jobTitle}</h4>
+            <h4 className={styles.hideOnMobile}>|</h4>
+            <h4>{jobExperience.employerName}</h4>
+          </div>
+          <div className={styles.cardHeaderRight}>
+            <h4>{formatDate(jobExperience.startDate)}</h4>
+            <h4 className={styles.hideOnMobile}>-</h4>
+            <h4>
+              {formatDate(jobExperience.endDate) === "01/01/1970"
+                ? "Present"
+                : formatDate(jobExperience.endDate)}
+            </h4>
+          </div>
+        </div>
+        <div className={styles.cardDescription}>
+          <p>{jobExperience.jobDescription}</p>
+        </div>
+        <div className={styles.cardFooter}></div>
+      </Card>
+    );
+  };
+
+  return (
+    <Panel header="Job Experience">
+      <div className={styles.container}>
+        <DataView
+          value={sortFunction(jobExperience)}
+          className={styles.dataViewContainer}
+          layout="grid"
+          rows={3}
+          header={jobExperienceHeader}
+          itemTemplate={itemTemplate}
+        ></DataView>
+      </div>
+    </Panel>
+  );
+};
+
+export default JobExperiencePanel;
