2e69c30669629601b4778e54ed51c5b3ee8aafab
Lukas Lee <jiawei3e1@gmail.com>
2023-10-03 20:16:39 +0800

    improved UI/UX for list of job listings by job apps on recruiter side

--- src/common/enums/enums.js
+++ src/common/enums/enums.js
@@ -4,7 +4,8 @@ const Enums = {
     CORPORATE: 'Corporate',
     JOBSEEKER: 'Job_Seeker',
     INACTIVE: 'Inactive',
-    ACTIVE: 'Active'
+    ACTIVE: 'Active',
+    PROCESSING: 'Processing'
 };
 
 export default Enums;
