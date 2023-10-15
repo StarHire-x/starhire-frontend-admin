415c62e04d00bfc1428232066a84c797132f387a
Shawn Lim <e0727020@u.nus.edu>
2023-10-15 18:56:32 +0800

    added resolveTicket API call

--- src/app/api/ticket/route.js
+++ src/app/api/ticket/route.js
@@ -43,3 +43,30 @@ export const viewOneTicket = async (ticketId, accessToken) => {
     throw error;
   }
 };
+
+export const resolveTicket = async (ticketId, accessToken) => {
+  try {
+    const res = await fetch(
+      `http://localhost:8080/ticket/resolve/${ticketId}`,
+      {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+        cache: 'no-store',
+      }
+    );
+
+    if (!res.ok) {
+      const errorData = await res.json();
+      console.log(errorData);
+      throw new Error(errorData.message);
+    }
+
+    return await res.json();
+  } catch (error) {
+    console.log('There was a problem resolving the ticket', error);
+    throw error;
+  }
+};
