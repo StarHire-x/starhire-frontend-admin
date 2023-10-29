0cce04b0bfa2302b7cb70b6d01fdf2b829d206e2
Lukas Lee <jiawei3e1@gmail.com>
2023-10-29 23:59:13 +0800

    update commission rate done

---
+++ src/app/commission/components/ManageCommissionRate/ManageCommissionRate.jsx
@@ -0,0 +1,88 @@
+import { Card } from "primereact/card";
+import styles from "./ManageCommissionRate.module.css";
+import { InputNumber } from "primereact/inputnumber";
+import { useState, useEffect, useRef } from "react";
+import { Button } from "primereact/button";
+import {
+  getAllCommissionRates,
+  updateCommissionRate,
+} from "@/app/api/commission/route";
+import { Toast } from "primereact/toast";
+
+const ManageCommissionRateModal = ({ accessToken }) => {
+  const [commissionRateObj, setCommissionRateObj] = useState({});
+  const toast = useRef(null);
+
+  const handleUpdateCommissionRate = async () => {
+    console.log("Commission Rate obj: " + JSON.stringify(commissionRateObj));
+
+    try {
+      await updateCommissionRate(
+        commissionRateObj,
+        commissionRateObj.commissionRateId,
+        accessToken
+      );
+      await toast.current.show({
+        severity: "success",
+        summary: "Success",
+        detail: "Successfully updated the commission rate!",
+        life: 5000,
+      });
+    } catch (err) {
+      // update commission rate here
+      toast.current.show({
+        severity: "error",
+        summary: "Error",
+        detail: "Failed to update the commission rate!",
+        life: 5000,
+      });
+    }
+  };
+
+  useEffect(() => {
+    getAllCommissionRates(accessToken).then((response) => {
+      if (response.length > 0) {
+        setCommissionRateObj(response[0]);
+      }
+    });
+  }, [accessToken]);
+
+  return (
+    <>
+      <Toast ref={toast} />
+      <Card>
+        <div className={styles.manageCommissionRateContainer}>
+          <p className={styles.commissionRateNote}>
+            Note that this commission rate is standardized for all recruiter's
+            commissions.
+          </p>
+
+          <div className={styles.commissionRateInput}>
+            <label
+              htmlFor="commissionRate"
+              className={styles.commissionRateLabel}
+            >
+              Commission Rate in (%):
+            </label>
+            <InputNumber
+              inputId="commissionRate"
+              value={commissionRateObj?.commissionRate}
+              onValueChange={(e) =>
+                setCommissionRateObj({
+                  commissionRateId: commissionRateObj.commissionRateId,
+                  commissionRate: e.value,
+                })
+              }
+              maxFractionDigits={2}
+            />
+          </div>
+          <div className={styles.commissionSaveBtnContainer}>
+            <Button label="Save" onClick={() => handleUpdateCommissionRate()} />
+          </div>
+        </div>
+      </Card>
+    </>
+  );
+};
+
+export default ManageCommissionRateModal;
--- src/app/commission/page.jsx
+++ src/app/commission/page.jsx
@@ -1,12 +1,48 @@
-'use client';
+"use client";
+import { Button } from "primereact/button";
 import styles from "./commission.module.css";
+import { Dialog } from "primereact/dialog";
+import ManageCommissionRateModal from "./components/ManageCommissionRate/ManageCommissionRate";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const CommissionPage = () => {
-    return (
-        <>
+  const session = useSession();
+  const router = useRouter();
+  if (session.status === "unauthenticated") {
+    router?.push("/login");
+  }
+
+  const accessToken =
+    session.status === "authenticated" &&
+    session.data &&
+    session.data.user.accessToken;
+
+  const [
+    manageCommissionRateModalVisibility,
+    setManageCommissionRateModalVisibility,
+  ] = useState(false);
+
+  return (
+    <>
+      <div className={styles.commissionHeaderContainer}>
         <h2>Commission</h2>
-        </>
-    )
+        <Button
+          size="small"
+          label="Manage Commission Rate"
+          onClick={() => setManageCommissionRateModalVisibility(true)}
+        />
+        <Dialog
+          header="Manage Commission Rate"
+          visible={manageCommissionRateModalVisibility}
+          onHide={() => setManageCommissionRateModalVisibility(false)}
+        >
+          <ManageCommissionRateModal accessToken={accessToken} />
+        </Dialog>
+      </div>
+    </>
+  );
 };
 
 export default CommissionPage;
